8ad2fe76dec4210ffb5e8273d68364db947bca96
Elvengard <sincemac@gmail.com>
2016-10-12 16:07:26 +0800

    merge branch

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var dialog = require("./dialog/dialog.js");
+
+var sum1 = new dialog({
+    num1:3,
+    num2:1
+    })
+sum1.sum();
+console.log("heelo");
+console.log("dsfas");
---
+++ output/output.js
@@ -0,0 +1,74 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var dialog = __webpack_require__(1);
+
+	var sum1 = new dialog({
+	    num1:3,
+	    num2:1
+	    })
+	sum1.sum();
+	console.log("heelo");
+	console.log("dsfas");
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = function(opt){
+		this.num1 = opt.num1;
+		this.num2 = opt.num2;
+		this.sum = function(){
+			console.log(this.num1+this.num2);
+		}
+
+
+	}
+
+
+/***/ }
+/******/ ]);
