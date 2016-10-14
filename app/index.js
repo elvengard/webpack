492299be174a8b2a8b62194e0261c41e7609b668
Elvengard <sincemac@gmail.com>
2016-10-14 16:54:33 +0800

    new demo

--- app/index.js
+++
@@ -1,9 +0,0 @@
-var dialog = require("./dialog/dialog.js");
-require("./index.less");
-var sum1 = new dialog({
-    num1:3,
-    num2:1
-    })
-sum1.sum();
-console.log("heelo");
-console.log("dsfas");
