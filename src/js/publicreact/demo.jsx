239616102851ceceb660c564d021b9478d008480
Elvengard <sincemac@gmail.com>
2016-10-18 22:00:11 +0800

    add react demo

---
+++ src/js/publicreact/demo.jsx
@@ -0,0 +1,12 @@
+var React = require ("react");
+var ReactDom = require("react-dom");
+
+
+module.exports = function(opt){
+    
+	ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('example')
+    );
+}
+
