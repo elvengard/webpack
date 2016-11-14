d0d67f91df847d726ff410a739335436509efe93
exsaber <sincemac@gmail.com>
2016-11-14 10:51:42 +0800

    change select module

--- src/js/new.js
+++ src/js/new.js
@@ -7,10 +7,32 @@ var dropdown = require("./publicreact/dropdown.jsx");
 
 
 var dropdownC1 = new dropdown({
-	dom:document.getElementById("dropdown1")
+	dom:document.getElementById("dropdown1"),
+	option:[
+				{name:"测试1",value:"测试2"},
+				{name:"测试2",value:"测试3"},
+				{name:"测试3",value:"测试4"},
+				{name:"测试4",value:"测试5"}
+			],
+	select:"--请选择--",
+	id:"C1",
+	onSelect:function(json){
+		console.log(json)
+	}
 });
 
 var dropdownC2 = new dropdown({
-	dom:document.getElementById("dropdown2")
+	dom:document.getElementById("dropdown2"),
+	option:[
+				{name:"100ml",value:"测试2"},
+				{name:"200ml",value:"测试3"},
+				{name:"300ml",value:"测试4"},
+				{name:"400ml",value:"测试5"}
+			],
+	select:"请选择规格",
+	id:"C2",
+	onSelect:function(json){
+		console.log(json);
+	}
 });
 
--- src/js/publicreact/dropdown.jsx
+++ src/js/publicreact/dropdown.jsx
@@ -4,30 +4,51 @@ var ReactDOM = require("react-dom");
 
 module.exports = function(opt){
 
-    var CommentBox = React.createClass({
+	var Optiontab = React.createClass({
+	render: function() {
+		var classId = this.props.idNumber;
+
+		var optionNodes = this.props.data.map(function (comment,index) {
+		      return  (<li key={index}><a href="javascript:;" className={"_module_select_" + classId }>{comment.name}</a></li>);
+    	});
+
+	    return (<ul className="dropdown-menu">{optionNodes}</ul>);
+
+	  }
+
+	});
+
+
+
+    var SelectBox = React.createClass({
 	render: function() {
 	    return (
 		    <div className="btn-group">
 			    <button data-toggle="dropdown" className="btn btn-default dropdown-toggle" aria-expanded="false">
-			    Action <span className="caret"></span>
+			    {this.props.select} <span className="caret"></span>
 
 			    </button>
-			    <ul className="dropdown-menu">
-			        <li><a href="buttons.html#">选项一</a></li>
-			        <li><a href="buttons.html#">Another action</a></li>
-			        <li><a href="buttons.html#">Something else here</a></li>
-			        <li className="divider"></li>
-			        <li><a href="buttons.html#">Separated link</a></li>
-			    </ul>
+			    
+			 	<Optiontab data={this.props.data} idNumber={this.props.idNumber}/>
+			    
 			</div>
 	    );
 	  }
 	});
 
+
 	ReactDOM.render(
-	  <CommentBox />,
+	  <SelectBox data={opt.option} select={opt.select} idNumber={opt.id}/>,
 	  opt.dom
 	);
+
+	$("._module_select_" + opt.id).click(function(){
+
+		var select_index = $(this).parent().index();
+		opt.onSelect(opt.option[select_index]);
+
+	})
+
 
 }
 
