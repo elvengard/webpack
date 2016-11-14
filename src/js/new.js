3abfabe263021f703acdf5fd179e18d38521b4df
exsaber <sincemac@gmail.com>
2016-11-14 13:55:25 +0800

    add dropdown module and fix bug

--- src/js/new.js
+++ src/js/new.js
@@ -8,18 +8,25 @@ var dropdown = require("./publicreact/dropdown.jsx");
 
 var dropdownC1 = new dropdown({
 	dom:document.getElementById("dropdown1"),
+	id:"C1",
+	title:"--请选择要的规格--",
 	option:[
 				{name:"测试1",value:"测试2"},
 				{name:"测试2",value:"测试3"},
 				{name:"测试3",value:"测试4"},
 				{name:"测试4",value:"测试5"}
 			],
-	select:"--请选择--",
-	id:"C1",
+	value:undefined,
+	
+
 	onSelect:function(json){
 		console.log(json)
+		dropdownC1.value = json.value;
 	}
+	
 });
+
+
 
 var dropdownC2 = new dropdown({
 	dom:document.getElementById("dropdown2"),
@@ -29,10 +36,19 @@ var dropdownC2 = new dropdown({
 				{name:"300ml",value:"测试4"},
 				{name:"400ml",value:"测试5"}
 			],
-	select:"请选择规格",
+	title:"请选择规格",
 	id:"C2",
 	onSelect:function(json){
 		console.log(json);
-	}
+		dropdownC2.value = json.value;
+	},
+	value:undefined
 });
+
+
+
+$("#bb_add_user").click(function(){
+	console.log(dropdownC1.value);
+	console.log(dropdownC2.value);
+})
 
--- src/js/publicreact/dropdown.jsx
+++ src/js/publicreact/dropdown.jsx
@@ -4,12 +4,22 @@ var ReactDOM = require("react-dom");
 
 module.exports = function(opt){
 
+
+
+
+	//react组件区-----------------------------------
 	var Optiontab = React.createClass({
 	render: function() {
 		var classId = this.props.idNumber;
 
 		var optionNodes = this.props.data.map(function (comment,index) {
-		      return  (<li key={index}><a href="javascript:;" className={"_module_select_" + classId }>{comment.name}</a></li>);
+		      return  (
+		      			<li key={index}>
+		      				<a href="javascript:;" className={"_module_select_" + classId }>
+		      					{comment.name}
+		      				</a>
+		      			</li>
+		      		   );
     	});
 
 	    return (<ul className="dropdown-menu">{optionNodes}</ul>);
@@ -19,14 +29,18 @@ module.exports = function(opt){
 	});
 
 
+	var bottonStyle = {minWidth: "100px" }
+	var titleStyle = {marginRight: "12px"}
+
 
     var SelectBox = React.createClass({
 	render: function() {
 	    return (
 		    <div className="btn-group">
-			    <button data-toggle="dropdown" className="btn btn-default dropdown-toggle" aria-expanded="false">
-			    {this.props.select} <span className="caret"></span>
-
+			    <button data-toggle="dropdown" className="btn btn-default dropdown-toggle" aria-expanded="false"
+			     style={bottonStyle}>
+				    <span className="_module_title" style={titleStyle}>{this.props.title}</span>
+				    <span className="caret"></span>
 			    </button>
 			    
 			 	<Optiontab data={this.props.data} idNumber={this.props.idNumber}/>
@@ -38,14 +52,18 @@ module.exports = function(opt){
 
 
 	ReactDOM.render(
-	  <SelectBox data={opt.option} select={opt.select} idNumber={opt.id}/>,
+	  <SelectBox data={opt.option} title={opt.title} idNumber={opt.id}/>,
 	  opt.dom
 	);
+	//react组件区----------------------------------------------------------------------------------------
+
+
 
 	$("._module_select_" + opt.id).click(function(){
 
 		var select_index = $(this).parent().index();
 		opt.onSelect(opt.option[select_index]);
+		$(this).parent().parent().parent().find("._module_title").text(opt.option[select_index].name);
 
 	})
 
